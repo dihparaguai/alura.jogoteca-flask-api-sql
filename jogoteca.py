a51761f731f9e9d0c89a75cfe09ddd016464b6bd
Dih Paraguai <diego.paraguai.dp@gmail.com>
2024-11-21 10:17:58 -0300

    add tela e rota login; add rota autenticar e manter dados em cookies; add rota logout e remover dados em cookies; import session e flash; uso da secret_key; add alert() personalizado no template.html

--- jogoteca.py
+++ jogoteca.py
@@ -2,15 +2,21 @@
 from modules.jogo import Jogo
 
 
-# 'render_template' por padrao olha os arquivos dentro do diretorio 'templates'
-# para obter os valores do formulario, foi importado 'resquest'
-# 'redirect' corresponde a uma biblioca com funcoes para redicionamento de paginas
-from flask import Flask, render_template, request, redirect
+# 'render_template' > por padrao olha os arquivos dentro do diretorio 'templates'
+# 'resquest' > captura os valores dos abritudos 'name' dos arquivos 'html'
+# 'redirect' > biblioca com funcoes para redicionamento de paginas
+# 'session' > cookies do navegador, que guardar algumas informacoes, como nome do usuario logado
+# 'flash' > passar texo e informacoes para 'alert()' no navegador de forma personalizada
+from flask import Flask, render_template, request, redirect, session, flash
 
 
 
-# '__name__' referencia o nome do proprio arquivo
+# 'Flask()' > classe que possui os comandos de http e api
+# '__name__' > referencia o nome do proprio arquivo
 app = Flask(__name__)
+
+# criptografa os cookies, para ser usando em conunto com 'session[]'
+app.secret_key = 'chave_secreta'
 
 
 
@@ -28,14 +34,14 @@ lista_jogos = [jogo1, jogo2, jogo3, jogo4]
 # rota da api
 @app.route('/')
 def index():
-    # 'render_template' funcao do flask para conectar com o 'html'
-    # 'titulo' corresponde a variavel criada com dupla-chaves no arquivo html '{{ }}'
-    # 'lista_jogos' sera enviado ao 'html' que vai iterar sobre ela utilizando 'foreach'
+    # 'render_template' > funcao do flask para conectar com o 'html'
+    # 'titulo' > variavel criada com dupla-chaves no arquivo html '{{ }}'
+    # 'lista_jogos' > sera enviado ao 'html' que vai iterar sobre ela utilizando 'foreach'
     return render_template('lista.html', titulo='jogos', jogos=lista_jogos)
 
 
 
-# rota de outra pagina html, com formulario de cadastro
+# rota de pagina html, com formulario de cadastro
 @app.route('/novo')
 def novo_jogo():
     return render_template('novo.html', titulo='novo jogo')
@@ -59,6 +65,40 @@ def criar_jogo():
 
     # retorno para a rota '/' (index)
     return redirect('/')
+
+
+
+# rota de pagina html, com formulario de login
+@app.route('/login')
+def login():
+    return render_template('/login.html')
+
+
+
+# rota de intermediaria, que valida as informacoes de login antes de passar para a pagina 'index'
+@app.route('/autenticar', methods=['POST',])
+def autenticar():
+    if 'alohomora' == request.form['senha']:
+        
+        # 'session' permite armazenar dados de cookies, mas para isso, precisa de '.secretkey' para criptografar os cookies
+        session['usuario_logado'] = request.form['usuario']
+        
+        # 'flash' permite mostrar um 'alert()' personalizado no navegador, usando as inforacoes que estavam nos cookies, devido ao 'session'
+        flash(session['usuario_logado'] + ' logado com sucesso')
+        return redirect('/')
+
+    else:
+        flash('usuario ou senha invalido')
+        return redirect('/login')
+
+
+# rota intermediaria, que remove as informacoes de usuario dos cookies para fazer 'logout'
+@app.route('/logout')
+def logout():
+    session['usuario_logado'] = None
+    flash('logout efetuado com sucesso')
+    return redirect('/login')
+
 
 
 # coloca o servidor flask para rodar
