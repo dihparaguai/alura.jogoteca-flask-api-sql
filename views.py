71ad6256a674764a3e42ac62a44204bac842dd28
Diego Paraguai <diego.paraguai.dp@gmail.com>
2024-12-19 07:45:21 -0300

    Ajustado rotas: 'criar()' para salvar imagens com timestamp, 'editar()' para recuperar a imagem salva, 'atualizar()' para substituir e deletar a imagem antiga, e 'deletar()' para excluir a imagem associada ao registro / Incluído 'helpers.py' com 'recupera_capa()' para retornar o nome da imagem e 'deleta_capa_antiga()' para excluir a capa (exceto padrão) / Adicionado input para imagem no 'editar.html'

--- views.py
+++ views.py
@@ -6,13 +6,18 @@
 # 'url_for' > permite passar a funcao que renderiza a pagina/rota html para o 'redirect()'
 # 'send_from_directory' > busca um arquivo/imagem em um diretorio para enviar para a pagina/rota
 from flask import render_template, request, redirect, send_from_directory, session, flash, url_for
+
+# 'time' > possui funcoes de data / hora
+import time
+
 from jogoteca import db, app
 from modules import Usuarios, Jogos
+from helpers import deleta_capa_antiga, recupera_capa
 
 # rota da api
 @app.route('/')
 def index():
-    
+
     # 'render_template' > funcao do flask para conectar com o 'html'
     # 'titulo' > variavel criada com dupla-chaves no arquivo html '{{ }}'
     # 'lista_jogos' > sera enviado ao 'html' que vai iterar sobre ela utilizando 'foreach'
@@ -26,7 +31,7 @@ def index():
 # rota de pagina html, com formulario de cadastro
 @app.route('/novo')
 def novo():
-    
+
     # se nao existir sessao ou se a chave 'usuario_logado' for igual a none
     # garante que se o usuario nao estiver loga, entao noa acessarava esta pagina
     if 'usuario_logado' not in session or session['usuario_logado'] == None:
@@ -46,21 +51,22 @@ def criar():
     # verifica se o jogo que sera cadastrado ja existe no banco de dados
     if Jogos.query.filter_by(nome=nome).first():
         return redirect(url_for('index'))
-    
+
     # instancia um objeto de banco de dados com as informacoes do cadastro do jogo
     novo_jogo = Jogos(nome=nome, categoria=categoria, console=console)
-    
+
     # adiciona o objeto do jogo ao banco de dados
     db.session.add(novo_jogo)
     db.session.commit()
-    
+
     # busca o arquivo atraves do 'resquest.files[]' e do 'name', para salvar na pasta 'uploads/'
     # o nome do arquivo a ser salvo contem o numero do id e nome do novo jogo adicionado
     arquivo = request.files['arquivo']
-    
-    # usa o 'app.config[]' do Flask para buscar no dicionario formado pelo nome das variaveis que estao no arquivo 'config.py', e assim utilizar os dados atribuitos em 'UPLOAD_PATH' 
-    upload_path = app.config['UPLOAD_PATH']
-    arquivo.save(f'{upload_path}/capa{novo_jogo.id}.jpg')
+
+    # usa o 'app.config[]' do Flask para buscar no dicionario formado pelo nome das variaveis que estao no arquivo 'config.py', e assim utilizar os dados atribuitos em 'UPLOAD_PATH'
+    upload_path = app.config['UPLOADS_PATH']
+    timestamp = time.time()
+    arquivo.save(f'{upload_path}/capa{novo_jogo.id}-{timestamp}.jpg')
 
     # retorno para a rota '/' (index)
     # url_for utiliza a funcao da rota ao inves pagina da rota
@@ -73,25 +79,40 @@ def criar():
 def editar(id):
     if 'usuario_logado' not in session or session['usuario_logado'] == None:
         return redirect('/login?proxima=editar')
-    
+
     # renderiza a pagina 'editar.html' com os dados do jogo que foi selecionado na pagina de lista
     jogo = Jogos.query.filter_by(id=id).first()
-    return render_template('editar.html', titulo='editar jogo', jogo=jogo)
+
+    capa_jogo = recupera_capa(id)
+    return render_template('editar.html', titulo='editar jogo', jogo=jogo, capa_jogo=capa_jogo)
 
 
 # rota intermediaria, que atualiza um item selecionado na lista de jogos
 @app.route('/atualizar', methods=['POST',])
 def atualizar():
-    
+
     # para atualizar o jogo, as novas informações sobrescrevem os dados que estavam no banco de dados
     jogo = Jogos.query.filter_by(id=request.form['id']).first()
     jogo.nome = request.form['nome']
     jogo.categoria = request.form['categoria']
     jogo.console = request.form['console']
-    
+
     # ao adicionar o jogo com o mesmo id, ele sobrescreve o que estava gravado no banco de dado
     db.session.add(jogo)
     db.session.commit()
+
+    arquivo = request.files['arquivo']
+    upload_path = app.config['UPLOADS_PATH']
+    
+    # retorna o momento atual
+    timestamp = time.time()
+    
+    # deleta o antigo antigo, para nao ficar arquivos orfãos armazenados
+    deleta_capa_antiga(jogo.id)
+    arquivo.save(f'{upload_path}/capa{jogo.id}-{timestamp}.jpg')
+
+    # retorno para a rota '/' (index)
+    # url_for utiliza a funcao da rota ao inves pagina da rota
     return redirect(url_for('index'))
 
 
@@ -100,11 +121,14 @@ def atualizar():
 def deletar(id):
     if 'usuario_logado' not in session or session['usuario_logado'] == None:
         return redirect('/login')
-    
+
     # deleta a linha com a chave do jogo selecionado, pra isso recebe o id do jogo atraves do 'url_for'
     Jogos.query.filter_by(id=id).delete()
-    db.session.commit()
     
+    # deleta o arquivo vinculado ao registro que também foi deletado
+    deleta_capa_antiga(id)
+    db.session.commit()
+
     flash('jogo deletado com sucesso')
     return redirect(url_for('index'))
 
@@ -112,10 +136,10 @@ def deletar(id):
 # rota de pagina html, com formulario de login
 @app.route('/login')
 def login():
-    
+
     # recupera a informacao que foi passada para a 'query string', ou seja, o valor que estiver na variavel 'proxima' na url
     proxima = request.args.get('proxima')
-    
+
     # renderiza a pagina login, ja passando a 'query string' que foi recebida pela variavel
     return render_template('login.html', proxima=proxima)
 
@@ -123,16 +147,17 @@ def login():
 # rota de intermediaria, que valida as informacoes de login antes de passar para a pagina 'index'
 @app.route('/autenticar', methods=['POST',])
 def autenticar():
-    
+
     # busca e filtra no banco de dados nome do usuario digitado no formulario html
-    usuario = Usuarios.query.filter_by(nickname=request.form['usuario']).first()
-    
+    usuario = Usuarios.query.filter_by(
+        nickname=request.form['usuario']).first()
+
     # caso o ususario exista...
     if usuario:
-        
-        # verifica se este usuario fitrado possui a mesma senha que foi digitada 
+
+        # verifica se este usuario fitrado possui a mesma senha que foi digitada
         if request.form['senha'] == usuario.senha:
-                
+
             # 'session' permite armazenar dados de cookies, mas para isso, precisa de '.secretkey' para criptografar os cookies
             session['usuario_logado'] = usuario.nickname
 
@@ -147,7 +172,7 @@ def autenticar():
 
     else:
         flash('usuario ou senha invalido')
-        
+
         # url_for utiliza a funcao da rota ao inves pagina da rota
         return redirect(url_for('login'))
 
@@ -157,7 +182,7 @@ def autenticar():
 def logout():
     session['usuario_logado'] = None
     flash('logout efetuado com sucesso')
-    
+
     # url_for utiliza a funcao da rota ao inves pagina da rota
     return redirect(url_for('login'))
 
