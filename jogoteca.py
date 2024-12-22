21104e66ab5bbdecdcb772f9385f3805faec88ad
Diego Paraguai <diego.paraguai.dp@gmail.com>
2024-12-22 10:39:52 -0300

    Adicionado 'csrf_token()' nos formulários das páginas 'novo.html' e 'editar.html' para segurança nos envios de dados.

--- jogoteca.py
+++ jogoteca.py
@@ -10,6 +10,9 @@ from flask import Flask
 # o 'ORM' utulizado sera o 'SQLAlchemy'
 from flask_sqlalchemy import SQLAlchemy
 
+# a partir do 'flask_wtf', deve-se utilizar o CSRF para garantir segurança em formularios web
+from flask_wtf.csrf import CSRFProtect
+
 # 'Flask()' > classe que possui os comandos de http e api
 # '__name__' > referencia o nome do proprio arquivo
 app = Flask(__name__)
@@ -20,7 +23,10 @@ app.config.from_pyfile('config.py')
 # inicializa o 'ORM' entre o banco de dados, atraves da classe 'SQLAlchemy', e a classe 'Flask', passando a variavel 'app' (classe 'Flask') como parametro para o 'ORM' na classe de 'SQLAlchemy'
 db = SQLAlchemy(app)
 
-#
+# funciona como um token, que gera numeros aleatorios, garantindo a segurança em formularios 'html'
+crf = CSRFProtect(app)
+
+# importa tudo que esta no arquivo 'views.py'
 from views import *
 
 # coloca o servidor flask para rodar
